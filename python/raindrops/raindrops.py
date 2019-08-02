f4e6d7ee911467df640c4843867f9ee8d7cfcb0e
Josiah Holmes <josiahkholmes@gmail.com>
2019-08-02 16:02:09 -0400

    Update dictionary values from string to integer

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,9 +1,9 @@
 def convert(number):
     factors = {
-        '3': 'Pling',
-        '5': 'Plang',
-        '7': 'Plong'
+        3: 'Pling',
+        5: 'Plang',
+        7: 'Plong'
     }
-    raindrop_speak = ''.join([factors[str(idx)] for idx in range(1, number+1)
-                              if ((number % idx == 0) and (str(idx) in factors))])
+    raindrop_speak = ''.join([factors[idx] for idx in range(1, number+1)
+                              if ((number % idx == 0) and (idx in factors))])
     return raindrop_speak if raindrop_speak else str(number)
