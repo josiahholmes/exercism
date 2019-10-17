0f0d0004e71d509a3420b28b089d0eccec02a566
Josiah Holmes <josiahkholmes@gmail.com>
2019-10-17 08:46:20 -0400

    Extra files

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -14,20 +14,23 @@ def less_than_equal_to_column(idx: int, column: List[int]) -> bool:
 
 
 def saddle_points(matrix: List[List]) -> List[dict]:
-    point = {}
     columns = []
     saddle_points = []
 
-    for idx in range(len(matrix)):
-        columns.append([row[idx] for row in matrix])
+    if matrix:
+        for idx in range(len(matrix)):
+            columns.append([row[idx] for row in matrix])
 
-    
-
-matrix = [[4, 5, 4], [3, 5, 5], [1, 5, 4]]
-# column = [[4, 3, 1], [5, 5, 5], [4, 5, 4]]
-'''
-4 5 4
-3 5 5
-1 5 4
-'''
-saddle_points(matrix)
+        for row in matrix:
+            for i in range(len(row)):
+                if greater_than_equal_to_row(i, row.copy()) and less_than_equal_to_column(i, columns[i].copy()):
+                    saddle_points.append(
+                        {"row": matrix.index(row) + 1, "column": i + 1})
+                else:
+                    saddle_points = [{}]
+        if {} not in saddle_points:
+            saddle_points = list(
+                {sp['row']: sp for sp in saddle_points}.values())
+    else:
+        saddle_points = [{}]
+    return saddle_points
